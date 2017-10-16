f99dfba02898eb12bcb1d2292d715353e1eccd93
udit <yudistrange@gmail.com>
2017-10-16 14:06:43 +0530

    Creates a separate log file for the plugin metrics

--- src/org/openstreetmap/josm/plugins/InstrumentedEventQueue.java
+++ src/org/openstreetmap/josm/plugins/InstrumentedEventQueue.java
@@ -1,19 +1,18 @@
 package org.openstreetmap.josm.plugins;
 
-import com.sun.nio.zipfs.ZipPath;
 import org.openstreetmap.josm.tools.Logging;
+import sun.rmi.runtime.Log;
 
-import java.awt.EventQueue;
 import java.awt.AWTEvent;
-import java.awt.event.ActionEvent;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.nio.Buffer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
+import java.awt.EventQueue;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * This class creates an alternative <code>EventQueue</code> which adds logging to every AWTEvent dispatched.
@@ -23,8 +22,28 @@ import java.nio.file.StandardOpenOption;
  */
 public class InstrumentedEventQueue extends EventQueue {
 
-    {
-        writer.write();
+    private static Properties pluginProperties = new Properties();
+    private static Logger pluginLogger = null;
+
+    // Set up logger for UI event instrumentation
+    static {
+        try {
+            // Load up the properties
+            InputStream inputStream = new FileInputStream("instrument-ui.properties");
+            pluginProperties.load(inputStream);
+
+            // Set up plugin logger
+            pluginLogger = Logger.getLogger(pluginProperties.getProperty("loggername", "instrument-ui-logger"));
+            FileHandler fileHandler = new FileHandler(pluginProperties.getProperty("logfile", "/tmp/instrument-ui.log"));
+            pluginLogger.addHandler(fileHandler);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+
+        } catch (FileNotFoundException e) {
+            Logging.error(e);
+        } catch (IOException ioEx) {
+            Logging.error(ioEx);
+        }
     }
 
     @Override
@@ -35,10 +54,10 @@ public class InstrumentedEventQueue extends EventQueue {
 
         /**
             TODO:
-            - Log to file instead of stdout
-            - Read the file location from the plugin configuration
+           - Read the file location from the plugin configuration
             - Support for different formats? (CSV, JSON)
          */
+        pluginLogger.info("Time: " + ((endNano - startNano) / 1000000) + "ms, Event Type: " + event);
         if (endNano - startNano > 50000000)
             System.out.println( "Time: " + ((endNano - startNano) / 1000000) + "ms, Event Type: " + event);
     }
