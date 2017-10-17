3d33237d0c6d5c661dbd7a531ed0d783f1b2a7a1
udit <yudistrange@gmail.com>
2017-10-17 14:01:54 +0530

    Filter only ActionEvent and MouseEvents

--- src/org/openstreetmap/josm/plugins/InstrumentationPlugin.java
+++ src/org/openstreetmap/josm/plugins/InstrumentationPlugin.java
@@ -2,6 +2,7 @@ package org.openstreetmap.josm.plugins;
 
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.tools.Logging;
 
 import java.awt.*;
 
@@ -17,5 +18,6 @@ public class InstrumentationPlugin extends Plugin {
         EventQueue customEventQueue = new InstrumentedEventQueue();
         EventQueue applicationQueue = Toolkit.getDefaultToolkit().getSystemEventQueue();
         applicationQueue.push(customEventQueue);
+        Logging.info("Replaced default event queue with a logging event queue");
     }
 }
--- src/org/openstreetmap/josm/plugins/InstrumentedEventQueue.java
+++ src/org/openstreetmap/josm/plugins/InstrumentedEventQueue.java
@@ -1,15 +1,13 @@
 package org.openstreetmap.josm.plugins;
 
+import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.tools.Logging;
-import sun.rmi.runtime.Log;
 
-import java.awt.AWTEvent;
-import java.awt.EventQueue;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -21,14 +19,16 @@ import java.util.logging.SimpleFormatter;
  * @author udit
  */
 public class InstrumentedEventQueue extends EventQueue {
+
     private static final String LOGGER_NAME = "instrument-ui-logger";
     private static final String LOGFILE_NAME = "/tmp/instrument-ui.log";
     private static Logger pluginLogger = null;
 
-    // Set up logger for UI event instrumentation
+    /*
+     *  Set up logger for UI event instrumentation
+     */
     static {
         try {
-            // Set up plugin logger
             pluginLogger = Logger.getLogger(LOGGER_NAME);
             FileHandler fileHandler = new FileHandler(LOGFILE_NAME);
             pluginLogger.addHandler(fileHandler);
@@ -40,16 +40,71 @@ public class InstrumentedEventQueue extends EventQueue {
         }
     }
 
+    /**
+     * Override the default event loop to generate event time logs
+     *
+     * @param event
+     */
     @Override
-    protected void dispatchEvent (AWTEvent event) {
+    protected void dispatchEvent(AWTEvent event) {
         long startNano = System.nanoTime();
         super.dispatchEvent(event);
         long endNano = System.nanoTime();
+        actionEventLogStatement(event, startNano, endNano);
+    }
 
-        /**
-         TODO:
-         - Support for different formats? (CSV, JSON)
-         */
-        pluginLogger.info("Time: " + (endNano - startNano) + "ns, Event Type: " + event);
+    /**
+     * Logs the time taken to perform action events
+     *
+     * @param event
+     * @param startNano
+     * @param endNano
+     *
+     * TODO:
+     * Support formats easier for aggregating (Don't know what it is?)
+     */
+    private void actionEventLogStatement(AWTEvent event, long startNano, long endNano) {
+        String eventMsg = getEventMsg(event);
+        if (eventMsg != null &&
+            !eventMsg.isEmpty()) {
+            pluginLogger.info(eventMsg + " : " + (endNano - startNano));
+        }
+    }
+
+    /**
+     * Given a parent Component and position, returns the bottom most component at that position
+     *
+     * @param parent
+     * @param posX
+     * @param posY
+     * @return
+     */
+    private Component getLeafLevelComponent (Component parent, int posX, int posY) {
+        Component child = parent.getComponentAt(posX, posY);
+        if (child == parent ||
+                child == null) {
+            return parent;
+        } else {
+            return getLeafLevelComponent(child, posX, posY);
+        }
+    }
+
+    private String getEventMsg (AWTEvent event) {
+        if (event instanceof ActionEvent) {
+            return ((ActionEvent) event).getActionCommand();
+        } else if ((event instanceof MouseEvent) &&
+                ((event.getID() == MouseEvent.MOUSE_RELEASED) ||
+                        (event.getID() == MouseEvent.MOUSE_CLICKED)) &&
+                (event.getSource() instanceof Component)) {
+
+            Component parentComponent = (Component) event.getSource();
+            Component leaf = getLeafLevelComponent(parentComponent, ((MouseEvent) event).getX(), ((MouseEvent) event).getY());
+
+            if (leaf instanceof JButton) {
+                JosmAction eventAction = (JosmAction) ((JButton) leaf).getAction();
+                return eventAction.getShortcut().getLongText();
+            }
+        }
+        return null;
     }
 }
